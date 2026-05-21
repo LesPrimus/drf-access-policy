021c67421f583943a5d3f7536a2f5221e790aff3
Aprimus <aprimus@tuxedo-stellaris.lan>
2026-05-21 08:26:07 +0200

    test: port test_view_set_mixin to pytest

--- test_project/testapp/tests/test_view_set_mixin.py
+++ test_project/testapp/tests/test_view_set_mixin.py
@@ -1,30 +1,29 @@
-from rest_framework.test import APITestCase
-
-from rest_access_policy import AccessViewSetMixin, AccessPolicy
-from rest_framework.viewsets import ViewSet
+import pytest
 from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ViewSet
+
+from rest_access_policy import AccessPolicy, AccessViewSetMixin
 
 
-class AccessViewSetTestCase(APITestCase):
-    def test_should_raise_error_if_no_access_policy_set(self):
-        class MyViewSet(AccessViewSetMixin, ViewSet):
-            pass
+def test_should_raise_error_if_no_access_policy_set():
+    class MyViewSet(AccessViewSetMixin, ViewSet):
+        pass
 
-        with self.assertRaises(Exception) as context:
-            v = MyViewSet()
+    with pytest.raises(Exception, match="you must assign an AccessPolicy "):
+        MyViewSet()
 
-        self.assertTrue("you must assign an AccessPolicy " in str(context.exception))
 
-    def test_should_not_raise_error_if_access_policy_set(self):
-        class MyViewSet(AccessViewSetMixin, ViewSet):
-            access_policy = AccessPolicy
+def test_should_not_raise_error_if_access_policy_set():
+    class MyViewSet(AccessViewSetMixin, ViewSet):
+        access_policy = AccessPolicy
 
-        v = MyViewSet()
+    MyViewSet()  # no exception
 
-    def test_prepend_policy_to_permissions_without_modifying_class_attribute(self):
-        class MyViewSet(AccessViewSetMixin, ViewSet):
-            access_policy = AccessPolicy
 
-        v = MyViewSet()
-        self.assertEqual(v.permission_classes, [AccessPolicy, AllowAny])
-        self.assertEqual(MyViewSet.permission_classes, [AllowAny])
+def test_prepend_policy_to_permissions_without_modifying_class_attribute():
+    class MyViewSet(AccessViewSetMixin, ViewSet):
+        access_policy = AccessPolicy
+
+    v = MyViewSet()
+    assert v.permission_classes == [AccessPolicy, AllowAny]
+    assert MyViewSet.permission_classes == [AllowAny]
